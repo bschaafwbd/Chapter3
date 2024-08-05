36b5b9d01b1b0a6dd427227b349564588d82aba6
Brian Schaaf <brian.schaaf@wbd.com>
2024-08-05 12:57:05 -0400

    make node app

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const words = [
+  "hello", "world", "hello", "world", "hello", "world", "hello", "world",]
+
+const test = () => {
+  for (let i = 0; i < words.length; i++) {
+    console.log(words[i])
+  }
+}
+
+test()
