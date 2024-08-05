85f064e75bb5cdc27b7bc00314890314d5a97fb6
Brian Schaaf <brian.schaaf@wbd.com>
2024-08-05 13:22:14 -0400

    added eslint/prettier configs

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const words = [
-  "hello", "world", "hello", "world", "hello", "world", "hello", "world",]
+  "hello", "world", "hello", "world", "hello", "world", "hello", "world"]
 
 const test = () => {
   for (let i = 0; i < words.length; i++) {
